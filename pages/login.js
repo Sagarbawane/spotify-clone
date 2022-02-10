4b1593c1372cf014519a53f5dedff4af8b9c2752
sagar bawane <sagar@iamclearmind.com>
2022-02-10 23:24:34 +0530

    changes

--- pages/login.js
+++ pages/login.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { getProviders, signIn } from 'next-auth/react'
 
 function Login({ providers }) {
+  console.log(providers)
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
       <img className="mb-5 w-52" src="https://links.papareact.com/9xl" alt="" />
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
+          {console.log(provider)}
           <button
             className="rounded-lg bg-[#18d860] p-5 text-white"
             onClick={() => {
